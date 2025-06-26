1ecca03369982f78460811277b00f1a13da70c07
Damir <damir.krkalic@yahoo.com>
2025-06-26 10:15:57 +0200

    Connect to Azure AI Foundry

---
+++ ai-foundry.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import ModelClient from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+  const client = new ModelClient(
+  process.env.AZURE_INFERENCE_SDK_ENDPOINT ?? "https://damir-mcc1mogb-eastus2.services.ai.azure.com/models", new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "YOUR_KEY_HERE"));
+
+var messages = [
+  { role: "developer", content: "You are an helpful assistant" },
+  { role: "user", content: "What are 3 things to see in Seattle?" },
+];
+
+var response = await client.path("chat/completions").post({
+  body: {
+    messages: messages,
+    max_completion_tokens: 800,
+      temperature: 1,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      model: "gpt-4.1",
+  },
+});
+
+console.log(JSON.stringify(response));
